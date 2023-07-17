a84db8fab938213537019d035cd7a3e81b8dd8f5
Lemon-tech24 <alisangcocarl@gmail.com>
2023-07-17 22:15:38 +0800

    just cors

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const cors = require('cors')
 
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT'); 
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
+app.use(cors())
 
 app.use(express.json());
 
