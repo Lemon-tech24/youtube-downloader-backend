c1d3276249ed963973841b72f344ed7899971466
Lemon-tech24 <alisangcocarl@gmail.com>
2023-07-17 23:09:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (_req, res) => {
   res.send('/api running');
 });
 
-app.post('/request', async (req, res) => {
+app.post('http://encouraging-pink-wig.cyclic.app/api/request', async (req, res) => {
   try {
     const { search } = req.body;
     const r = await yts(search);
@@ -36,7 +36,7 @@ app.post('/request', async (req, res) => {
   }
 });
 
-app.post('/download', async (req, res) => {
+app.post('http://encouraging-pink-wig.cyclic.app/api/download', async (req, res) => {
   try {
     const { videoId } = req.body;
     const { formats } = await ytdl.getInfo(
@@ -54,7 +54,7 @@ app.post('/download', async (req, res) => {
   }
 });
 
-app.post('/download/audio', async (req, res) => {
+app.post('http://encouraging-pink-wig.cyclic.app/api/download/audio', async (req, res) => {
   try {
     const { videoId } = req.body;
     const { formats } = await ytdl.getInfo(
